4066ea829fdec8db17f21b16606864e29a15e4ce
Juan Sebastian <juan_seb07@hotmail.com>
2022-06-24 14:41:54 -0500

    Se agregan mas botones con nuevas funcionalidades.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,25 @@ export class AppComponent {
   number1:number = 0;
   number2:number = 0;
   resultado:number = 0;
+  text:string = '';
 
   sumar(){
     this.resultado = this.number1 + this.number2;
+    this.text = 'el resultado de la suma es: ';
+  }
+
+  restar(){
+    this.resultado = this.number1 - this.number2;
+    this.text = 'el resultado de la resta es: ';
+  }
+
+  dividir(){
+    this.resultado = this.number1 / this.number2;
+    this.text = 'el resultado de la division es: ';
+  }
+
+  multiplicar(){
+    this.resultado = this.number1 * this.number2;
+    this.text = 'el resultado de la multiplicacion es: ';
   }
 }
